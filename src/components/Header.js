a7f3990d51a12aa5b121da91f6214e2a085aa2a7
maximiloi <maximiloi@gmail.com>
2021-03-11 10:57:36 +0300

    изменил импорт модуля.сss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import './Header.scss';
+import * as headerStyles from './Header.module.scss';
 
 export default function Header() {
     return (
-        <header className="header">
+        <header className={headerStyles.header}>
             <h1>
-                <Link className="title" to='/'>
+                <Link className={headerStyles.title} to='/'>
                     Maxim iloi
                 </Link>
             </h1>
             <nav>
-                <ul className="nav-list">
-                    <li><Link className="nav-item" activeClassName="nav-item-active" to='/'>Home</Link></li>
-                    <li><Link className="nav-item" activeClassName="nav-item-active" to='/blog'>Blog</Link></li>
-                    <li><Link className="nav-item" activeClassName="nav-item-active" to='/about'>About</Link></li>
-                    <li><Link className="nav-item" activeClassName="nav-item-active" to='/contact'>Contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to='/'>Home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to='/blog'>Blog</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to='/about'>About</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to='/contact'>Contact</Link></li>
                 </ul>
             </nav>
         </header>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import '../css/index.scss';
-import './Layout.scss';
+import * as layoutStyles from './Layout.module.scss';
 
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout(props) {
     return (
-        <div className='container'>
-            <div className='content'>
+        <div className={layoutStyles.container}>
+            <div className={layoutStyles.content}>
                 <Header />
                 {props.children}
             </div>
