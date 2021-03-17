6e881010e97ee74360b0aaffb076e34a01ef4507
maximiloi <maximiloi@gmail.com>
2021-03-17 09:41:25 +0300

    все посты готовы

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/Layout';
 
@@ -12,6 +12,9 @@ export default function BlogPage() {
                         frontmatter {
                             title 
                             date
+                        }
+                        fields {
+                            slug
                         }
                     }
                 }
@@ -26,8 +29,10 @@ export default function BlogPage() {
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
                         <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
+                            <Link to={`/blog/${edge.node.fields.slug}`}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                                <p>{edge.node.frontmatter.date}</p>
+                            </Link>
                         </li>
                     )
                 })}
